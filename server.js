7dcc0532963bff4d32a62a7573a9eed10d46402f
MaddieBurns12 <mburns6023@gmail.com>
2022-08-11 15:04:23 -0400

    search animals by ID

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -52,7 +57,12 @@ app.get('/api/animals', (req, res) => {
 });
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
-    res.json(result);
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
