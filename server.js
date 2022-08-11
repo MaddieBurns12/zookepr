c1909707cb33a365df025f052fc8fe1500119409
MaddieBurns12 <mburns6023@gmail.com>
2022-08-11 14:40:15 -0400

    debugging

--- server.js
+++ server.js
@@ -50,6 +50,10 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 })
+app.get('/api/animals', (req, res) => {
+    const result = findByIq(req.params.id, animals);
+    res.json(result);
+})
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
