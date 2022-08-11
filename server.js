d6116e596435fca8233a81fb8d8fd06810294086
MaddieBurns12 <mburns6023@gmail.com>
2022-08-11 16:42:08 -0400

    added data validation

--- server.js
+++ server.js
@@ -67,6 +67,23 @@ function createNewAnimal (body, animalsArray) { //!!!!//
     return animal;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -87,11 +104,17 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be 
     req.body.id = animals.length.toString();
 
+    //if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
     // add animal to json file and animals array in this function
     const animal = createNewAnimal(req.body, animals); 
 
     //req.body is where our incoming content will be
     res.json(animal);
+    }
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
